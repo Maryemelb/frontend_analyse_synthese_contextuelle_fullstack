52b5627379129886242f9d497b22d50be6dc8916
maryem <maryemelbergui@gmail.com>
2025-12-11 22:18:54 +0100

    SC-12:create design for my app and add dockerfile

--- app/Auth/login/page.jsx
+++ app/Auth/login/page.jsx
@@ -13,7 +13,7 @@ export default function Login() {
         try {
 
             let response = await fetch(
-                'http://127.0.0.1:8000/Auth/login', {
+               ` ${process.env.NEXT_PUBLIC_BACKEND_API}/Auth/login`, {
                 method: 'POST',
                 headers: {"Content-Type": "application/json"},
                 credentials: "include",
@@ -25,6 +25,7 @@ export default function Login() {
 
             if (!response.ok) {
                 console.log('error')
+                router.push('/login')
                 return;
             }
           
@@ -39,9 +40,36 @@ export default function Login() {
     }
     return (
         <div>
-            <h1>Login</h1>
+            {/* <h1>Login</h1>
             <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} placeholder="username" /><br />
             <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="password" />
-            <button onClick={submit}>submit</button>
+            <button onClick={submit}>submit</button> */}
+
+            <section className="bg-gray-50 dark:bg-gray-900 ">
+                <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+                    
+                    <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 shadow-3xl  sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+                        <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+                            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                                Login
+                            </h1>
+                            <form className="space-y-4 md:space-y-6 " onSubmit={submit} action="#">
+                                <div>
+                                    <label htmlFor="username" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+                                    <input type="username" name="username" id="username" onChange={(e)=>setUsername(e.target.value)} className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required=""/>
+                                </div>
+                                <div>
+                                    <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                                    <input type="password" name="password" id="password" placeholder="••••••••" onChange={(e)=>setPassword(e.target.value)} className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required=""/>
+                                </div>
+                                <button type="submit" className="w-full font-bold text-white bg-orange-500 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
+                                <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+                                    Don't have an account! <a href="login" className="text-primary-600 hover:underline dark:text-primary-500">sign up</a>
+                                </p>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </div>)
 }
